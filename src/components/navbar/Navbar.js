d8b8afa2cac5b67744cf21c4f53e6082b1631386
Julian <Van Ost>
2020-07-29 20:50:11 -0700

    fixes app so that it runs- actually this time. Removes faulty link tags

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {
-  Link
-} from 'react-router-dom'
-const Navbar = () => {
+import './navbar.css'
+
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Navbar = props => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <span className="navbar-brand mb-1 h1">Julian Van Ost</span>
@@ -10,16 +11,24 @@ const Navbar = () => {
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <Link className="nav-item nav-link" to='/'>Home</Link>
-          <Link className="nav-item nav-link" to='/about'>About</Link>
-          <Link className="nav-item nav-link" to='/portfolio'>Portfolio</Link>
-          <Link className="nav-item nav-link" to='/contact'>Contact</Link>
-        </div>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="nav navbar-nav navbar-right">
+          <li className="nav-item">
+            <a className="nav-link" href="./index.html">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./about.html">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./portfolio.html">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./contact.html">Contact</a>
+          </li>
+        </ul>
       </div>
+      <h1>{props.person}</h1>
     </nav>
-
   )
 }
 
